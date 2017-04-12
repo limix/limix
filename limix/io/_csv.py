677c8469e74c3ba14e06eff07124e2416c43ee36
Danilo Horta <danilo.horta@gmail.com>
2017-04-12 18:33:40 +0100

    Csv data

--- limix/io/_csv.py
+++ limix/io/_csv.py
@@ -1,6 +1,26 @@
 from dask.dataframe import read_csv as _read_csv
 
 def read_csv(filename):
+    r"""Read a CSV file.
+
+    Args:
+        filename (str): path to a CSV file.
+
+    Returns:
+        pandas.DataFrame: return a dataframe representation.
+
+    Example
+    -------
+
+        .. doctest::
+
+            >>> from limix.io import read_csv
+            >>> from limix.io.examples import csv_file_example
+            >>>
+            >>> df = read_csv(csv_file_example())
+            >>> print(df)
+
+    """
     df = _read_csv(filename)
     df.set_index(df.columns[0], inplace=True)
     return df
