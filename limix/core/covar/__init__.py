ed36fa1cb96831c14dd205362c728ac50e558a21
Danilo Horta <danilo.horta@gmail.com>
2017-04-18 15:43:50 +0100

    Save qtl changes, and vardecomp

--- limix/core/covar/__init__.py
+++
@@ -1,4 +0,0 @@
-from limix_legacy.core.covar import (CategoricalLR, Cov2KronSum, Cov2KronSumLR,
-                                     Cov3KronSumLR, Covariance, DiagonalCov,
-                                     FixedCov, FreeFormCov, KronCov,
-                                     LowRankCov, SQExpCov, SumCov, ZKZCov)
