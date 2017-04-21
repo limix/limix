a717cfa8e011348f780601a3a02b679ef29c469b
Danilo Horta <danilo.horta@gmail.com>
2017-04-21 13:53:51 +0100

    Rubric references

--- limix/iSet/__init__.py
+++ limix/iSet/__init__.py
@@ -5,6 +5,10 @@ Interaction Set Test (iSet)
 
 - :func:`.fit_iSet`
 
+.. rubric:: References
+
+.. [CHRS17] Casale FP, Horta D, Rakitsch B, Stegle O (2017) Joint genetic analysis using variant sets reveals polygenic gene-context interactions. PLOS Genetics 13(4): e1006693.
+
 Public interface
 ^^^^^^^^^^^^^^^^
 """
--- limix/mtSet/__init__.py
+++ limix/mtSet/__init__.py
@@ -5,6 +5,10 @@ Multi Trait Set Test (mtSet)
 
 - :class:`.MTSet`
 
+.. rubric:: References
+
+.. [CHRS17] Casale FP, Rakitsch B, Lippert C, Stegle O (2015) Efficient set tests for the genetic analysis of correlated traits. Nat Methods Aug;12(8): 755-8.
+
 Public interface
 ^^^^^^^^^^^^^^^^
 """
