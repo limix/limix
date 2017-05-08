8ff11452579373c1ddc2d332055280bda7f30465
fpcasale <casale@ebi.ac.uk>
2017-05-08 09:27:59 +0100

    new version release

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def setup_package():
 
     metadata = dict(
         name='limix',
-        version='1.0.2',
+        version='1.0.3',
         maintainer="Limix Developers",
         maintainer_email="horta@ebi.ac.uk",
         author=("Christoph Lippert, Danilo Horta, " +
