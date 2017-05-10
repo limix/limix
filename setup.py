5f132e5b2dd078010d1539b18c61f26b6a858796
Danilo Horta <danilo.horta@gmail.com>
2017-05-10 10:47:16 +0100

    New pandasplink

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def setup_package():
     setup_requires = ["cython", "numpy"] + pytest_runner
     install_requires = [
         'scikit-learn', 'limix-core>=1.0.1',
-        'dask[array,dataframe,delayed,bag]',
+        'dask[array,bag,dataframe,delayed]>=0.14',
         'h5py', 'pandas-plink>=1.1.6', 'limix-legacy', 'glimix-core>=1.2.4',
         'joblib>=0.11', 'tqdm>=4.10', 'scipy>=0.18'
     ]
