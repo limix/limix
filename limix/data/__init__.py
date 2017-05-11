393de37a44ddcbd427f1b5bd439ba89ef03d37c6
fpcasale <casale@ebi.ac.uk>
2017-05-11 16:19:46 +0100

    added query helper class

--- limix/data/__init__.py
+++ limix/data/__init__.py
@@ -4,12 +4,14 @@ Dataset utils
 *************
 
 - :class:`.BedReader`
+- :class:`.QueryList`
 
 Public interface
 ^^^^^^^^^^^^^^^^
 """
 
 from .bed_reader import BedReader
+from .query import QueryList
 from .geno_iterator import GIter
 
-__all__ = ['BedReader']
+__all__ = ['BedReader', 'QueryList']
