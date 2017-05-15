ab03929e6b955c95cc70b84fc76ce3803c04896c
Danilo Horta <danilo.horta@gmail.com>
2017-05-15 18:25:03 +0100

    require newer dep versions

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ def setup_package():
     install_requires = [
         'scikit-learn', 'limix-core>=1.0.1',
         'dask[array,bag,dataframe,delayed]>=0.14', 'h5py',
-        'pandas-plink>=1.1.7', 'limix-legacy', 'glimix-core>=1.2.5',
+        'pandas-plink>=1.1.7', 'limix-legacy', 'glimix-core>=1.2.6',
         'joblib>=0.11', 'tqdm>=4.10', 'scipy>=0.18', 'distributed',
-        'numpy-sugar>=1.0.38', 'ncephes>=1.0.26', 'asciitree>=0.3.3'
+        'numpy-sugar>=1.0.38', 'ncephes>=1.0.37', 'asciitree>=0.3.3'
     ]
     tests_require = ['pytest', 'pytest-console-scripts']
 
