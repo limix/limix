2bfc26afe237cff1a4ba58ebb38413c90dbd2c95
Danilo Horta <danilo.horta@gmail.com>
2017-10-04 13:14:57 +0100

    yes test

--- limix/scripts/__init__.py
+++ limix/scripts/__init__.py
@@ -1,8 +1,1 @@
-from . import iset_analyze
-from . import iset_postprocess
-from . import limix_converter
-from . import limix_runner
-from . import mtset_analyze
-from . import mtset_postprocess
-from . import mtset_preprocess
-from . import mtset_simpheno
+from . import limix_converter, limix_runner
