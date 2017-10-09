629d9ffbd83270aea7a8935418af08a8e4b28101
Danilo Horta <danilo.horta@gmail.com>
2017-10-09 10:32:36 +0100

    color

--- limix/fprint.py
+++ limix/fprint.py
@@ -1,12 +1,34 @@
 from __future__ import print_function
 
 import sys
+from colors import color
 
 
-# TODO: add wprint, document it, and make sure everywhere is using those
 def oprint(*args, **kwargs):
+    r"""Info print piped to stdout.
+
+    Flush immediately.
+    """
     print(*args, file=sys.stdout, flush=True, **kwargs)
 
 
+def wprint(*args, **kwargs):
+    r"""Warning print piped to stderr.
+
+    Flush immediately.
+    """
+    msg = str(*args)
+    print(color(msg, style='bold'), file=sys.stderr, flush=True, **kwargs)
+
+
 def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, flush=True, **kwargs)
+    r"""Error print piped to stderr.
+
+    Flush immediately
+    """
+    msg = str(*args)
+    print(
+        color(msg, fg='red', style='bold'),
+        file=sys.stderr,
+        flush=True,
+        **kwargs)
