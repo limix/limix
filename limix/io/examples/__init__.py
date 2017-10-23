c4e48ade86e37a1d896eacc21b8333664420a072
Danilo Horta <danilo.horta@gmail.com>
2017-10-23 13:14:48 +0100

    no 100G on git anymore

--- limix/io/examples/__init__.py
+++ limix/io/examples/__init__.py
@@ -14,8 +14,3 @@ def csv_file_example():
 def gen_file_example():
     dir_path = dirname(realpath(__file__))
     return join(dir_path, 'data', 'gen', 'example')
-
-
-def numpy_kinship_file_example():
-    dir_path = dirname(realpath(__file__))
-    return join(dir_path, 'data', 'numpy', '1000G_kinship.npy')
