9946e259dd162e9ad6defe5a5cb3384fe1792c52
Danilo Horta <danilo.horta@gmail.com>
2018-08-02 15:57:38 +0100

    doctestplus

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     console_scripts = ["limix = limix.cmdlimix:entry"]
     setup(
         dependency_links=[
-            "git+https://github.com/horta/pytest-doctestplus.git@precision#egg=pytest-doctestplus-0.1.4.dev"
+            "git+https://github.com/horta/pytest-doctestplus.git@precision#egg=pytest-doctestplus-0.1.4.dev0"
         ],
         entry_points=dict(console_scripts=console_scripts),
     )
