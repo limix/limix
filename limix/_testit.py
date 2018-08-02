10ea0ef6adf2b4f622eab48bf835d81389e64cd5
Danilo Horta <danilo.horta@gmail.com>
2018-08-02 17:00:42 +0100

    travis

--- limix/_testit.py
+++ limix/_testit.py
@@ -12,13 +12,7 @@ def test(verbose=True):
         Exit code: ``0`` for success.
     """
 
-    args = [
-        "--doctest-modules",
-        "-x",
-        "--doctest-plus",
-        "--ignore=conftest.py",
-        "--ignore=limix/conftest.py",
-    ]
+    args = ["--doctest-modules", "-x", "--doctest-plus"]
     if not verbose:
         args += ["--quiet"]
 
