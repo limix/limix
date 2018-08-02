34ab9e8b57952f49d3c87ecda71c389348d18293
Danilo Horta <danilo.horta@gmail.com>
2018-08-02 22:51:22 +0100

    dep doctest

--- setup.py
+++ setup.py
@@ -2,9 +2,4 @@ from setuptools import setup
 
 if __name__ == "__main__":
     console_scripts = ["limix = limix.cmdlimix:entry"]
-    setup(
-        dependency_links=[
-            "http://github.com/horta/pytest-doctestplus/tarball/precision#egg=pytest-doctestplus-0.1.4.dev0"
-        ],
-        entry_points=dict(console_scripts=console_scripts),
-    )
+    setup(entry_points=dict(console_scripts=console_scripts))
