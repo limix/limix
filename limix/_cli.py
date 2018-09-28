85d291a0a6cb5d8ba8a4967c70a51b24a6c2016a
Danilo Horta <danilo.horta@gmail.com>
2018-09-28 19:25:42 +0100

    -m Command line for gwas analysis.

--- limix/_cli.py
+++ limix/_cli.py
@@ -151,3 +151,79 @@ def remove(ctx, filepath):
     import limix
 
     limix.sh.remove(filepath)
+
+
+@cli.command()
+@click.pass_context
+@click.argument("phenotypes-file")
+@click.argument("genotype-file")
+@click.option(
+    "--covariates-file",
+    help="Specify the file path to a file containing the covariates.",
+    default=None,
+)
+@click.option(
+    "--kinship-file",
+    help="Specify the file path to a file containing the kinship matrix.",
+    default=None,
+)
+@click.option(
+    "--likelihood",
+    help=(
+        "Specify the type of likelihood that will described"
+        " the residual error distribution."
+    ),
+    default="normal",
+)
+@click.option(
+    "--filter",
+    help=(
+        "Filtering expression to select which phenotype, genotype loci, and covariates"
+        " to use in the analysis."
+    ),
+    default="normal",
+)
+@click.option("--output-dir", help="Specify the output directory path.", default=None)
+def scan(ctx, genotype_file, output_dir):
+    """Perform genome-wide association scan.
+
+    This analysis requires minimally the specification of one phenotype
+    (PHENOTYPES_FILE) and genotype data (GENOTYPE_FILE).
+
+    The --filter option allows for selecting a subset of the original dataset for
+    the analysis. For example,
+
+        --filter="genotype: (chrom == '3') & (pos > 100) & (pos < 200)"
+
+    states that only loci of chromosome 3 having a position inside the range (100, 200)
+    will be considered. The --filter option can be used multiple times in the same
+    call. In general, --filter accepts a string of the form
+
+        <DATA-TYPE>: <BOOL-EXPR>
+
+    where <DATA-TYPE> can be phenotype, genotype, or covariate. <BOOL-EXPR> is a boolean
+    expression involving row or column names. Please, consult `pandas.DataFrame.query`
+    function from Pandas package for further information.
+    \f
+
+    Examples
+    --------
+
+    ... doctest::
+
+        # First we perform a quick file inspection. This step is optional but is very
+        # useful to check whether `limix` is able to read them and print out their
+        # metadata.
+        limix show phenotypes.csv
+        limix show genotype.bgen
+        limix show kinship.raw
+
+        # We now perform the analysis, specifying the genotype loci and the phenotype
+        # of interest.
+        limix phenotypes.csv genotype.bgen --kinship-file=kinship.raw \
+            --output-dir=results \
+            --filter="phenotype[:, 'height']" \
+            --filter="genotype[:, (chrom=='3') & (pos > 100000) & (pos < 200000)]"
+    """
+    # scan(G, y, lik, K=None, M=None, verbose=True)
+    pass
