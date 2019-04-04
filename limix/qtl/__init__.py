9bd840936d3bca17ca33863062a0c63250ff5970
Danilo Horta <danilo.horta@gmail.com>
2019-04-04 16:46:26 +0100

    del test qtl

--- limix/qtl/__init__.py
+++ limix/qtl/__init__.py
@@ -2,10 +2,7 @@
 Quantitative trait locus analysis.
 """
 
-
 from ._mt_scan import mt_scan
-from ._st_iscan import st_iscan
-from ._st_scan import st_scan
 from ._st_sscan import st_sscan
 
-__all__ = ["st_scan", "st_iscan", "st_sscan", "mt_scan"]
+__all__ = ["st_sscan", "mt_scan"]
