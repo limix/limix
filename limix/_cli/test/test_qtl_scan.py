bece73359283162a4a2f648b3c0dc8b293b9a4cf
Danilo Horta <danilo.horta@gmail.com>
2019-07-01 11:16:12 +0100

    depend on remfiles

--- limix/_cli/test/test_qtl_scan.py
+++ limix/_cli/test/test_qtl_scan.py
@@ -1,3 +1,4 @@
+import pytest
 from click.testing import CliRunner
 from numpy.testing import assert_equal
 
@@ -5,84 +6,99 @@ from limix import cli
 from limix.sh import download
 
 
-def test_cli_qtl_scan():
+def _urls(suffixes):
+    base = "http://rest.s3for.me/limix/"
+    return [base + s for s in suffixes]
 
+
+@pytest.mark.remfiles(_urls(["trait.csv"]))
+def test_cli_qtl_scan_no_variant(remfiles):
+    remfiles.chdir()
+    invoke = CliRunner().invoke
+    r = invoke(cli, ["qtl", "scan", "--trait", "trait.csv"])
+    assert_equal("Error: no variant has been specified." in r.stdout, True)
+    assert_equal(r.exit_code, 2)
+
+
+@pytest.mark.remfiles(_urls(["trait.csv", "plink.bed"]))
+def test_cli_qtl_scan_trait_bed(remfiles):
+    remfiles.chdir()
+    invoke = CliRunner().invoke
+    r = invoke(cli, ["qtl", "scan", "--bfile", "plink", "--trait", "trait.csv"])
+    assert_equal(type(r.exception), FileNotFoundError)
+    assert_equal(r.exit_code, 1)
+
+
+@pytest.mark.remfiles(_urls(["trait.csv", "plink.bed", "plink.bim"]))
+def test_cli_qtl_scan_trait_bed_bim(remfiles):
+    remfiles.chdir()
+    invoke = CliRunner().invoke
+    r = invoke(cli, ["qtl", "scan", "--bfile", "plink", "--trait", "trait.csv"])
+    assert_equal(type(r.exception), FileNotFoundError)
+    assert_equal(r.exit_code, 1)
+
+
+@pytest.mark.remfiles(_urls(["trait.csv", "plink.bed", "plink.bim", "plink.fam"]))
+def test_cli_qtl_scan_trait_bed_bim_fam(remfiles):
+    remfiles.chdir()
     runner = CliRunner()
-    with runner.isolated_filesystem():
-        invoke = runner.invoke
-        download("http://rest.s3for.me/limix/trait.csv", verbose=False)
-        result = invoke(cli, ["qtl", "scan", "--trait", "trait.csv"])
-        assert_equal(result.exit_code, 2)
+    r = runner.invoke(cli, ["qtl", "scan", "--bfile", "plink", "--trait", "trait.csv"])
+    assert_equal(r.exit_code, 0)
 
-        download("http://rest.s3for.me/limix/plink.bed", verbose=False)
-        result = invoke(
-            cli, ["qtl", "scan", "--bfile", "plink", "--trait", "trait.csv"]
-        )
-        assert_equal(type(result.exception), FileNotFoundError)
-        assert_equal(result.exit_code, 1)
 
-        download("http://rest.s3for.me/limix/plink.bim", verbose=False)
-        result = invoke(
-            cli, ["qtl", "scan", "--bfile", "plink", "--trait", "trait.csv"]
-        )
-        assert_equal(type(result.exception), FileNotFoundError)
-        assert_equal(result.exit_code, 1)
+@pytest.mark.remfiles(
+    _urls(
+        [
+            "rel/plink2.rel.bin",
+            "rel/plink2.rel.id",
+            "grm-bin/plink.grm.N.bin",
+            "grm-bin/plink.grm.bin",
+            "grm-bin/plink.grm.id",
+            "trait.csv",
+        ]
+    )
+)
+def test_cli_qtl_scan_grm_rel(remfiles):
+    remfiles.chdir()
+    runner = CliRunner()
+    r = runner.invoke(
+        cli,
+        [
+            "qtl",
+            "scan",
+            "--bfile",
+            "plink",
+            "--rel",
+            "plink2.rel.bin",
+            "--grm",
+            "plink.grm.bin",
+            "--trait",
+            "trait.csv",
+        ],
+    )
+    msg = "Error: The options [--grm, --rel] are mutually exclusive."
+    assert_equal(msg in r.stdout, True)
+    assert_equal(r.exit_code, 1)
 
-        download("http://rest.s3for.me/limix/plink.fam", verbose=False)
-        result = invoke(
-            cli, ["qtl", "scan", "--bfile", "plink", "--trait", "trait.csv"]
-        )
-        assert_equal(result.exit_code, 0)
 
-        result = invoke(cli, ["qtl", "scan", "--trait", "trait.csv"])
-        assert_equal("Error: no variant has been specified." in result.stdout, True)
-        assert_equal(result.exit_code, 2)
-
-        result = invoke(cli, ["qtl", "scan", "--bfile", "plink"])
-        assert_equal("Error: no trait has been specified." in result.stdout, True)
-        assert_equal(result.exit_code, 2)
-
-        download("http://rest.s3for.me/limix/rel/plink2.rel.bin", verbose=False)
-        download("http://rest.s3for.me/limix/rel/plink2.rel.id", verbose=False)
-
-        download("http://rest.s3for.me/limix/grm-bin/plink.grm.N.bin", verbose=False)
-        download("http://rest.s3for.me/limix/grm-bin/plink.grm.bin", verbose=False)
-        download("http://rest.s3for.me/limix/grm-bin/plink.grm.id", verbose=False)
-        result = invoke(
-            cli,
-            [
-                "qtl",
-                "scan",
-                "--bfile",
-                "plink",
-                "--rel",
-                "plink2.rel.bin",
-                "--grm",
-                "plink.grm.bin",
-                "--trait",
-                "trait.csv",
-            ],
-        )
-        assert_equal(
-            "Error: The options [--grm, --rel] are mutually exclusive."
-            in result.stdout,
-            True,
-        )
-        assert_equal(result.exit_code, 1)
-        result = invoke(
-            cli,
-            [
-                "qtl",
-                "scan",
-                "--bfile",
-                "plink",
-                "--trait",
-                "trait.csv",
-                "--method=st",
-                "--trait-name",
-                "pinto,pepeca",
-            ],
-        )
-        ok = "Error: not all specified traits have been found." in result.stdout
-        assert_equal(ok, True)
-        assert_equal(result.exit_code, 2)
+@pytest.mark.remfiles(_urls(["plink.bed", "plink.bim", "plink.fam", "trait.csv"]))
+def test_cli_qtl_scan_traits_not_found(remfiles):
+    remfiles.chdir()
+    runner = CliRunner()
+    r = runner.invoke(
+        cli,
+        [
+            "qtl",
+            "scan",
+            "--bfile",
+            "plink",
+            "--trait",
+            "trait.csv",
+            "--method=st",
+            "--trait-name",
+            "pinto,pepeca",
+        ],
+    )
+    ok = "Error: not all specified traits have been found." in r.stdout
+    assert_equal(ok, True)
+    assert_equal(r.exit_code, 2)
